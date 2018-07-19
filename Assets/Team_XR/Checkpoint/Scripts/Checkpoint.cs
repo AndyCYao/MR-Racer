e45b00b89ec8a3a2f1be2db53db4a5bdea0db9a4
Andy Y <andyyao3@gmail.com>
2018-07-19 13:43:51 -0700

    - Merge Andy_checkpoint branch via cherry picking. - Tweaked checkpoint and checkpoint manager script: 	- Checkpoint manager object is added onto the screen, roughly 1.7 m above the scanned scene transform position 	- Checkpoint manager will check the normal vector of the hit point in order to decide if it is a roughly horizontal plane instead of checking for height. 	- Checkpoint now has a index field, once hit by the player, it'll make an event that alert checkpointManager. - Checkpoint material is now red to make it easier to spot, the material is also a place holder for the real material later on.

---
+++ Assets/Team_XR/Checkpoint/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    public int index;
+    bool isHit = false;
+
+    public delegate void CheckpointPassed (int index);
+    public static event CheckpointPassed CheckpointPassedEvent;
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.CompareTag("Player"))
+        {
+            isHit = true;
+
+            CheckpointPassedEvent(index);
+        }
+	}
+}
---
+++ Assets/Team_XR/Checkpoint/Scripts/CheckpointManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+
+
+   // public GameObject plane;
+    public GameObject checkpointPrefab;
+    public int numberOfCheckpoints;
+
+    [SerializeField]
+    List<GameObject> checkpoints;
+    int remainingCheckPoints = 0;
+  
+
+
+    private void Awake()
+    {
+
+        //rayCastPoint = transform.position + transform.up * 2;
+        //   int numOfCheckpointsLeft = numberOfCheckpoints;
+        checkpoints = new List<GameObject>();
+    }
+
+	private void Reset()
+	{
+        remainingCheckPoints = 0;
+
+        Checkpoint.CheckpointPassedEvent += OnCheckpointReached;
+
+        while (remainingCheckPoints < numberOfCheckpoints)
+
+        {
+            CreateCheckpointOnFloor();
+            //remainingCheckPoints++;
+        }
+	}
+
+
+	private void Start()
+	{
+        Reset();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+
+    private void CreateCheckpointOnFloor()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray();
+        ray.origin = transform.position;
+        
+        bool hasCreated = false;
+        while(!hasCreated){
+            ray.direction = Random.insideUnitSphere;
+            if(Physics.Raycast(ray, out hit) && 
+            hit.transform.gameObject.layer == 8)
+            {
+                Debug.Log("hit x\t" + hit.point.x + " hit y\t" + hit.point.y +
+                        " hit z\t" + hit.point.z);
+                
+                if(Vector3.Angle (hit.normal, Vector3.up) < 10f)    // .2 on y is considered floor height
+                {
+                    checkpoints.Add (
+                        Instantiate(checkpointPrefab, hit.point, Quaternion.identity)
+                    );
+                    checkpoints[remainingCheckPoints].name = string.Format("Checkpoint_{0}", remainingCheckPoints);
+                    checkpoints[remainingCheckPoints].GetComponent<Checkpoint>().index = remainingCheckPoints;
+ 
+                    remainingCheckPoints++;
+                    hasCreated = true;
+                }
+            }            
+        }
+    }
+
+    void OnCheckpointReached (int index) {
+        remainingCheckPoints--;
+        Debug.Log(string.Format("CheckpointManager - OnCheckpointReached: {0}", index));
+        if (remainingCheckPoints == 0) {
+            Debug.Log("All check points are collected yay");
+            Reset();
+        }
+    }
+}
