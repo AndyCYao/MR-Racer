d3ee4fdb3e3230888f97c1626c0c15ecc3e4bbc2
Viet Phan <phan_t1@denison.edu>
2018-07-23 12:58:33 -0700

    - Added Thalita's checkpoint VFX into the scene

--- Assets/Team_XR/Checkpoint/Scripts/Checkpoint.cs
+++ Assets/Team_XR/Checkpoint/Scripts/Checkpoint.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
     public int index;
-    bool isHit = false;
+    public bool isHit = false;
 
     public delegate void CheckpointPassed (int index);
     public static event CheckpointPassed CheckpointPassedEvent;
 
 	private void OnTriggerEnter(Collider other)
 	{
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isHit )
         {
             isHit = true;
 
             CheckpointPassedEvent(index);
+
+            SetActive(false);
         }
 	}
+
+    public void SetActive (bool isActive) {
+
+        if (isActive ){
+            StartCoroutine (Activate());
+        }
+        else {
+            StartCoroutine (Deactivate());
+        }
+    }
+
+    IEnumerator Activate () {
+        yield return new WaitForSeconds(0.5f);
+        SetActive(true);
+    }
+
+    IEnumerator Deactivate()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SetActive(false);
+    }
 }
