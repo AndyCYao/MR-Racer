bfc74d975ab7c0745dd023195af2d41e43cb7e85
Viet Phan <phan_t1@denison.edu>
2018-07-30 14:03:54 -0700

    + Result display UI is added. Press trigger to move forward and reset the game

---
+++ Assets/Team_XR/Game/Scripts/States/Result/ResultState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Appl.UI;
+
+namespace Appl.State
+{
+    public class ResultState : State
+    {
+        protected UIMessagePanel uiMessagePanel;
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+
+
+            uiMessagePanel = UIManager.Instance.GetUIComponent("MessagePanel").GetComponent<UI.UIMessagePanel>();
+
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+        }
+    }
+}
